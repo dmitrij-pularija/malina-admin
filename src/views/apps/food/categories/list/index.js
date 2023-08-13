a3d0770e43bb86ae3a0a36f9a488b1b539700a16
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-08-13 14:36:47 +0300

    add category active function

--- src/views/apps/food/categories/list/index.js
+++ src/views/apps/food/categories/list/index.js
@@ -1,9 +1,11 @@
 import Table from "./Table"
+import Breadcrumbs from '@components/breadcrumbs'
 import "@styles/react/apps/app-users.scss"
 
 const CategoriesList = () => {
   return (
     <div className="app-user-list">
+      <Breadcrumbs title='Категории' data={[{ title: 'Структура' }, { title: 'Категории' }]} /> 
       <Table />
     </div>
   )
